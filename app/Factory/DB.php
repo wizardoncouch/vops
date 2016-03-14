b069df750ef91c5058a95b1c1ed918fe1765d760
Alex Culango <wizardoncouch@gmail.com>
2016-03-15 07:05:32 +0800

    PDO extension with config

--- app/Factory/DB.php
+++ app/Factory/DB.php
@@ -12,14 +12,22 @@ class DB extends \PDO
      */
     public function __construct()
     {
-        $dbDriver = getenv('DB_DRIVER');
-        $dbHost = getenv('DB_HOST');
-        $dbName = getenv('DB_NAME');
+        try {
+            $dbDriver = getenv('DB_DRIVER');
+            $dbHost = getenv('DB_HOST');
+            $dbName = getenv('DB_NAME');
 
-        $dsn = $dbDriver . ':host=' . $dbHost . ';dbname=' . $dbName . ';charset=utf8';
-        $usr = getenv('DB_USER');
-        $pwd = getenv('DB_PASSWORD');
-        parent::__construct($dsn, $usr, $pwd);
+            $dsn = $dbDriver . ':host=' . $dbHost . ';dbname=' . $dbName . ';charset=utf8';
+            $usr = getenv('DB_USER');
+            $pwd = getenv('DB_PASSWORD');
+            parent::__construct($dsn, $usr, $pwd);
+        } catch (\PDOException $e) {
+            header('HTTP/1.0 410 Gone');
+            die($e->getMessage());
+        } catch (\Exception $e) {
+            header('HTTP/1.0 500 Internal Server Error');
+            die($e->getMessage());
+        }
     }
 
 }
